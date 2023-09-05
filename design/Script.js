c16582baaf55279dc0877529133aef596ffab2c6
Gaurav <grv33431@gmail.com>
2023-09-05 21:11:56 +0530

    completeddd🤞🤞🤞🤞

--- design/Script.js
+++ design/Script.js
@@ -5,19 +5,22 @@ let button = document.querySelector(".button");
 // addEventListener takes two arguments: event and function ("click", function () {})
 button.addEventListener("click", validation);
 function validation() {
-  if (emailInput.value.contains("@") && emailInput.value.contains(".com")) {
-    //    you have 2 options that I can think of now
-    //    1. you can create 2 new pages one to show if the email is valid and one to show if the email is invalid
-    //    2. you can create a pop up that shows if the email is valid or not
-    //    if you follow the second method
-    //    you have to first create the html element here and depending up if the user has entered a valid email or not
-    //    you have to show either succes modl or error modal
-    // const oldDiv = document.querySelector(".class of old div");
-    const elemnt = document.createElement("div");
-    document.body.appendChild(elemnt);
-    oldDiv.classList.add(".hide"); // .hide {display: none}
+  if (emailInput.value.includes("@") && emailInput.value.includes(".com")) {
+    let ctr = document.querySelector(".container");
+    ctr.style.display = "none";
+    let suc = document.querySelector(".success");
+    suc.style.display = "block";
   } else {
-    const elemnt = document.createElement("div");
-    document.body.appendChild(elemnt);
+    let err = document.querySelector(".redemail");
+    err.style.display = "block";
   }
+}
+
+let btn = document.querySelector(".button2");
+btn.addEventListener("click", dismiss);
+function dismiss() {
+  let suc = document.querySelector(".success");
+  suc.style.display = "none";
+  let ctr = document.querySelector(".container");
+  ctr.style.display = "flex";
 }
